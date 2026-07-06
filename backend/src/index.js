df612c67deaa60d14745f1d24369d2544a0a90d6
Prajapati_Darshan <vp941334@gmail.com>
2026-07-06 17:38:43 +0530

    feat: implement Clerk webhook for user creation and deletion handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,38 +1,46 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config" 
+import "dotenv/config";
 import connectDB from "./lib/db.js";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import fs from "fs";
 import path from "path";
 import job from "./lib/cron.js";
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
-
 const pubDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
-app.use(cors({
-    origin : FRONTEND_URL,
-    credentials :true
-}));
+
+app.use("/api/webhooks/clerk",express.raw({type:"application/json"}), clerkWebhook)
+
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
 app.use(clerkMiddleware());
 
-if(fs.existsSync(pubDir)){
-    app.use(express.static(pubDir));
-    app.get("/{*any}", (req, res, next) => {
-        res.sendFile(path.join(pubDir, "index.html"), (err) => next(err));
-    })
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true });
+});
+
+if (fs.existsSync(pubDir)) {
+  app.use(express.static(pubDir));
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(pubDir, "index.html"), (err) => next(err));
+  });
 }
 
-app.listen(3000, ()=>{
-    connectDB();
-    console.log(`Server is running on PORT ${PORT}`);
+app.listen(3000, () => {
+  connectDB();
+  console.log(`Server is running on PORT ${PORT}`);
 
-    if(process.env.NODE_ENV === "production") {
-            job.start();
-    }
-})
+  if (process.env.NODE_ENV === "production") {
+    job.start();
+  }
+});
